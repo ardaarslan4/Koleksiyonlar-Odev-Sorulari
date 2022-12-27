32a28277f17bcff14bad4e03f46db12d54715377
Arda Arslan <47052871+ardaarslan4@users.noreply.github.com>
2022-12-27 08:30:53 +0300

    Add files via upload

---
+++ KoleksiyonlarIkinciOdev/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace KoleksiyonlarIkinciOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan,
+            her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını
+            console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)*/
+            int[] sayiDizisi = new int[6];
+            Console.WriteLine("Lütfen 20 adet pozitif sayi giriniz:");
+            
+            for (int i =0; i<20; i++)
+            {
+                Console.WriteLine("Lütfen {0}.sayiyi giriniz:",i+1);
+                //int sayilar = int.Parse(Console.ReadLine());
+                //sayiDizisi.Append<int>(int.Parse(Console.ReadLine()));
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Array.Sort(sayiDizisi);
+            Console.WriteLine(string.Join(",", sayiDizisi));
+
+            int sumEnBuyukSayilar=0;
+            int sumEnKucukSayilar =0;
+
+            for(int i=19; i>16; i--)
+            {
+                sumEnBuyukSayilar += sayiDizisi[i];
+                Console.WriteLine(sumEnBuyukSayilar);
+            }
+
+            for(int i=0; i<3; i++)
+            {
+                sumEnKucukSayilar += sayiDizisi[i];
+                Console.WriteLine(sumEnKucukSayilar);
+
+            }
+            int OrtSumEnBuyukSayilar = sumEnBuyukSayilar/3;
+            int OrtSumEnKucukSayilar = sumEnKucukSayilar/3;
+            double OrtSumEnBuyukEnKucukSayilar = (double)(sumEnBuyukSayilar+sumEnKucukSayilar)/(double)6;
+
+            Console.WriteLine("En Büyük 3 Sayinin Ortalamasi:");
+            Console.WriteLine(OrtSumEnBuyukSayilar);
+            Console.WriteLine("En Küçük 3 Sayinin Ortalamasi:");
+            Console.WriteLine(OrtSumEnKucukSayilar);
+            Console.WriteLine("En Büyük 3 ve En Küçük 3 Sayinin Ortalamasi:");
+            Console.WriteLine(OrtSumEnBuyukEnKucukSayilar);
+    
+        }
+    }
+}
---
+++ KoleksiyonlarUcuncuOdev/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace KoleksiyonlarUcuncuOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan 
+            ve dizinin elemanlarını sıralayan programı yazınız.*/
+            Console.WriteLine("Lütfen bir cümle giriniz:");
+            string text = Console.ReadLine();
+       
+            int countA =0;
+            int countE =0;
+            int countI =0;
+            int countO =0;
+            int countU =0;
+            for(int i=0; i<text.Length; i++)
+            {
+                if(text[i]=='a')
+                {
+                    countA++;
+                }
+                else if(text[i]=='e')
+                {
+                    countE++;
+                }
+                else if(text[i]=='i')
+                {
+                    countI++;
+                }
+                else if(text[i]=='o')
+                {
+                    countO++;
+                }
+                else if(text[i]=='u')
+                {
+                    countU++;
+                }
+                
+            }
+
+            Console.WriteLine($"Cümle içerisinde {countA} tane A, {countE} tane E, {countI} tane I, {countO} tane O ve {countU} tane U harfi vardir.");
+            int sumOfCounts=countA+countE+countI+countO+countU;
+            Console.WriteLine($"Cümle içerisindeki toplam sesli harf {sumOfCounts}");
+
+            int[] indexOfA = new int[sumOfCounts];
+            int[] indexOfE = new int[sumOfCounts];
+            int[] indexOfI = new int[sumOfCounts];
+            int[] indexOfO = new int[sumOfCounts];
+            int[] indexOfU = new int[sumOfCounts];
+
+            for (int i=0; i<sumOfCounts; i++)
+            {
+                indexOfA [i] = text.IndexOf('a');
+                indexOfE [i] = text.IndexOf('e');
+                indexOfI [i] = text.IndexOf('i');
+                indexOfO [i] = text.IndexOf('o');
+                indexOfU [i] = text.IndexOf('u');
+            }
+
+            char[] array = new char[sumOfCounts];
+            Console.WriteLine("Cümle içerisindeki sesli harflerin siralanmiş hali:");
+            for(int i=0; i<countA; i++)
+            {
+               
+                array[i] = text[indexOfA[i]];
+         
+            }
+            for(int i=0; i<countE; i++)
+            {
+               
+                array[i=countA] = text[indexOfE[i]];
+         
+            }
+            for(int i=0; i<countI; i++)
+            {
+               
+                array[i=countA+countE] = text[indexOfI[i]];
+         
+            }
+            for(int i=0; i<countO; i++)
+            {
+               
+                array[i=countA+countE+countI] = text[indexOfO[i]];
+         
+            }
+            for(int i=0; i<countU; i++)
+            {
+               
+                array[i=sumOfCounts-countU]=  text[indexOfU[i]];
+         
+            }
+            
+            Console.WriteLine(string.Join(",", array));
+            
+            //Console.WriteLine(countA*'a'+countE*'e'+countI*'i'+countO*'o'+countU*'u');
+
+        }
+    }
+}
